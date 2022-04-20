6c1c286ebc2145fd4c56aef6551fdb96fd58f2d9
jeffspatafora <jeffspatafora@gmail.com>
2022-04-20 12:28:56 -0700

    runs seeds file to seed db, adds cars controller, adds index action/route

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Car.create!([
+  {make: "dodge", model: "spirit", year: 1992, color: "light blue", image_url: "https://storage.googleapis.com/rp-production-public-content/j59ah4elilfewpa1v1kc63b0ugkf" },
+  {make: "oldsmobile", model: "acheiva", year: 1993, color: "maroon", image_url: "https://mercury.bid.cars/1-60010121/1992-Oldsmobile-Achieva-1G3NL5436NM424104-2.jpg" },
+  {make: "subaru", model: "forester", year: 2021, color: "gray", image_url: "https://smartcdn.prod.postmedia.digital/driving/wp-content/uploads/2021/04/Subaru-Forester-2021-1.jpg" }, 
+])
