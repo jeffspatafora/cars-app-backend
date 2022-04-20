f02413cd75e46fdd1b5d187c5a73f3ff0be06317
jeffspatafora <jeffspatafora@gmail.com>
2022-04-20 13:05:23 -0700

    adds create action

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -3,4 +3,16 @@ class CarsController < ApplicationController
     cars = Car.all
     render json: cars.as_json
   end
+
+  def create
+    car = Car.new(
+      make: params[:make],
+      model: params[:model],
+      year: params[:year],
+      color: params[:color],
+      image_url: params[:image_url]
+    )
+    car.save
+    render json: car.as_json
+  end
 end
